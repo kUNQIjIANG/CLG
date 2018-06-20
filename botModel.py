a84e43c08db24c0637b9b74c3be7074202ee077c
kunqi <ucabkj0@ucl.ac.uk>
2018-06-20 23:39:47 +0100

    save

--- botModel.py
+++ botModel.py
@@ -1,2 +1,18 @@
-import numpy as np
+import tensorflow as tf 
+
+class botModel:
+	def __init__(self,scope):
+		self.scope = scope
+		self.saver = tf.train.Saver()
+	
+	def save(self,sess,path):
+		self.saver.save(sess,path)
+		print("saved " + self.scope)
+
+	def load(self,sess,path):
+		self.saver.restore(sess,path)
+		print("loaded " + self.scope)
+		
+
+
 
