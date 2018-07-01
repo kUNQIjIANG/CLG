df15ada0e4fb7a8da0ba33acfa762a299dd6d8e1
kunqi <ucabkj0@ucl.ac.uk>
2018-07-01 15:53:36 +0100

    discri LSTM

--- discriminator.py
+++ discriminator.py
@@ -2,21 +2,24 @@ import tensorflow as tf
 import botModel
 
 class Discriminator(botModel):
-	def __init__(self,c_size):
+	def __init__(self,c_size, hid_units):
 		self.super.__init__('discriminator')
 		self.c_size = c_size
-		
+		self.hid_units = hid_units
 		self.build_graph()
 		
 	def build_graph(self):
 		with tf.variable_scope(self.scope):
-			self.enc_state = tf.placeholders(tf.float32, shape = [None, None])
-			self.proj_c = tf.layers.dense(self.c_size, tf.nn.relu)
+			self.enc_inp = tf.placeholders(tf.float32, shape = [None, None, None])
+			self.enc_len = tf.placeholders(tf.float32, shape = [None])
+			self.true_labels = tf.placeholders(tf.float32, shape = [None, None])
+			self.encoder = tf.nn.rnn_cell.BasicLSTMCell(self.hid_units)
+			self.enc_output, self.enc_final_state = tf.nn.dynamic_rnn(encoder_cell, enc_input, dtype = tf.float32, sequence_length = enc_len)
+			self.pred_c = tf.layers.dense(self.enc_final_state, self.c_size, tf.nn.relu)
+			self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = self.true_labels, logits = self.pred_c))
+	
+	def predict_c(self, sess, enc_inp, enc_len):
+		return sess.run(self.pred_c, {self.enc_inp: enc_inp, self.enc_len: enc_len})
 
-	def predict_c(self,enc_state):
-		return self.proj_c(enc_state)
-
-	def discrimi_loss(self, enc_state, true_label):
-		pred_c = self.predict_c(enc_state)
-		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = true_label, logits = pred_c))
-		return cross_entropy 
+	def discrimi_loss(self, enc_inp, enc_len, true_labels):
+		return sess.run(self.cross_entropy, {self.enc_inp : enc_inp, self.enc_len : enc_len, self.true_labels: true_labels})
--- trainer.py
+++ trainer.py
@@ -4,14 +4,16 @@ import Generator
 import Discriminator
 
 class Trainer:
-	def __init__(self, hid_units, batch_size, vocab_size, c_size):
+	def __init__(self, hid_units, batch_size, vocab_size, embed_size, c_size, word_embeds):
 
 		self.hid_units = hid_units
 		self.batch_size = batch_size
 		self.vocab_size = vocab_size
+		self.embed_size = embed_size
 		self.c_size = c_size 
+		self.word_embeds = word_embeds
 		self.build_graph()
-
+             
 	def build_graph(self):
 
 		self.encoder = Encoder(self.hid_units)
@@ -24,14 +26,18 @@ class Trainer:
 		enc_outputs = self.encoder.output_logits(self,sess,enc_input,enc_len)
 		reconst_loss = self.generator.reconst_loss(sess, enc_len, dec_len, z, enc_outputs, dec_tar, dec_input)
 		logits, gen_sen, sample_c = self.generator.outputs(sess, enc_len, dec_len, z, enc_outputs, dec_tar, dec_input)
-		new_z = self.encoder.encode(sess,logits,dec_len)
-		c_loss = self.discriminator.discrimi_loss(new_z, sample_labels)
+		
+		logits = tf.reshape(logits, [-1, self.vocab_size])
+		logit2word_embeds = tf.matmul(logits, self.word_embeds)
+		logit_encode = tf.reshape(logit2word_embeds, [self.batch_size,-1,self.embed_size])
+
+		c_loss = self.discriminator.discrimi_loss(logit_encode, dec_len, sample_c)
 		z_loss = tf.mutual_information(z,new_z)
 
 		generator_loss = reconst_loss + c_loss + z_loss
 		sess.run(self.optimize(generator_loss))
 		return gen_sen, sample_c
-		
+
 	def optimize(self,loss):
 		optimizer = tf.train.AdamOptimizer(1e-3)
     	gradients, variables = zip(*optimizer.compute_gradients(loss))
